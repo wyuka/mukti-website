f02f365f5a4ec6704953f056a9c0c87077fc0100
Tirtha Chatterjee <tirtha.p.chatterjee@gmail.com>
2012-12-30 04:34:21 +0530

    Loading from JS object and clicking on items implemented

---
+++ js/contents.js
@@ -0,0 +1,37 @@
+muktiObject = {
+    'text': 'Mukti',
+    'children': [
+        {
+        	'text': 'Workshops',
+            'line': 20,
+            'children': [
+            	{
+                	'text': 'Python',
+                    'line': 20,
+                    'children' : []
+                }, {
+                	'text': 'Web Development',
+                    'line': 30,
+                    'children' : []
+                }, {
+                	'text': 'Free Software',
+                    'line': 70,
+                    'children' : []
+                }, {
+                	'text': 'GSOC Barcamp',
+                    'line': 80,
+                    'children' : []
+                }
+            ]
+		}, {
+			'text': 'Talks',
+            'line': 30
+        }, {
+        	'text': 'Code',
+			'line': 70
+        }, {
+        	'text': 'Masti',
+            'line': 80
+        }
+    ]
+};
--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
-function showChild(text, line, id, leftSide) {
+var slowTransSpeed = 1000;
+
+function showChild(text, Y, id, leftSide) {
 	var X;
     if (leftSide == true) {
     	X = 2 + Math.floor(Math.random() * 43);
 	} else {
 		X = 45 + Math.floor(Math.random() * 35);
 	}
-	var Y = line * 10;
-	$('#childNodes').append('<div class="linkText" id="' + id + '" style="left: ' + X + '%; top: ' + Y + '%">' + text + '</div>');
+	$('#childNodes').append('<div class="linkText slowTrans invisible" id="' + id + '" style="left: ' + X + '%; top: ' + Y + '%" onclick="console.log(JSON.stringify(currentObject)); clickedOn(\'' + id + '\')">' + text + '</div>');
 	var item = $('#' + id);
+	setTimeout(function() {
+		item.removeClass('invisible');
+	}, 0)
+	setTimeout(function() {
+		item.removeClass('slowTrans');
+		item.addClass('fastTrans');
+	}, slowTransSpeed);
 	var leftX = item.position().left;
 	console.log(leftX);
 	function animateLink(link, right) {
@@ -22,4 +30,52 @@ function showChild(text, line, id, leftSide) {
 	} else {
 		animateLink(item, false);
 	}
+}
+
+var muktiObject, currentObject;
+
+function drawChildren () {
+	var object = currentObject;
+	for (var childNum = 0; childNum < object.children.length; childNum++) {
+		childText = object.children[childNum].text;
+		var onLeft, childLine;
+		if (childNum % 2)
+			onLeft = true;
+		else
+			onLeft = false;
+		childLine = object.children[childNum].line;
+
+		showChild(childText, childLine, childNum, onLeft);
+	}
+}
+
+function clickedOn(child) {
+	console.log(child);
+	var item = currentObject.children[child];
+	console.log(JSON.stringify(item));
+	currentObject = item;
+	eraseParent();
+	eraseChildren();
+}
+
+function eraseParent() {
+	$('#parentNode').css('opacity', 0);
+	setTimeout(drawParent, slowTransSpeed);
+}
+function eraseChildren() {
+	$('.linkText').removeClass('fastTrans');
+	$('.linkText').addClass('slowTrans');
+	$('.linkText').css('opacity', 0);
+	setTimeout(removeChildren, slowTransSpeed);
+	
+}
+
+function drawParent() {
+	parentNode = $('#parentNode');
+	parentNode.html(currentObject.text);
+	parentNode.css('opacity', 1);
+}
+function removeChildren() {
+	$('.linkText').remove();
+	drawChildren();
 }
