093f2d753563e83c7332f62c70fe35e51aa2cf9b
Tirtha Chatterjee <tirtha.p.chatterjee@gmail.com>
2012-12-31 00:49:42 +0530

    Changed transition type. No longer random[F

--- js/main.js
+++ js/main.js
@@ -6,19 +6,17 @@ function showChild(text, Y, id, leftSide) {
 	} else {
 		X = Math.floor(width / 2 + 30 + Math.random() * (width / 2 - 420));
 	}
-	$('#childNodes').append('<div class="linkText invisible" id="' + id + '" style="left: ' + X + 'px; top: ' + Y + '%" onclick="console.log(JSON.stringify(currentObject)); clickedOn(\'' + id + '\')">' + text + '</div>');
+	$('#childNodes').append('<div class="linkText invisible" id="' + id + '" style="left: ' + X + 'px; top: ' + Y + '%"">' + text + '</div>');
 	var item = $('#' + id);
 
-	var speed = getRandomSpeed();
-	setTransSpeed(item, speed + 'ms');
+	item.click( function() {
+		clickedOn(id);
+	});
+
 	setTimeout(function() {
 		item.removeClass('invisible');
 	}, 0)
-	setTimeout(function() {
-		setTransNormal(item);
-	}, speed);
 	var leftX = item.position().left;
-	console.log(leftX);
 	function animateLink(link, right) {
 		if (right == true) {
 			link.animate({"left": "+=25px"}, 2000 + Math.floor(Math.random() * 2000), function() { animateLink(link, false); });
@@ -49,14 +47,9 @@ function drawChildren() {
 	}
 }
 
-function getRandomSpeed() {
-	return (100 + 400 * Math.floor(Math.random() * 3));
-}
-
 function clickedOn(child) {
 	console.log(child);
 	var item = contents[child];
-	console.log(JSON.stringify(item));
 	currentObject = item;
 	eraseChildren();
 	eraseParent();
@@ -67,15 +60,8 @@ function eraseParent() {
 }
 
 function eraseChildren() {
-	var maxSpeed = 0;
-	$('.linkText').each(function() {
-		var speed = getRandomSpeed();
-		maxSpeed = Math.max(maxSpeed, speed);
-		setTransSpeed($(this), speed + 'ms');
-		$(this).css('opacity', 0);
-	});
-	console.log(maxSpeed);
-	setTimeout(removeChildren, maxSpeed + 1);
+	$('.linkText').css('opacity', 0);
+	setTimeout(removeChildren, 500);
 }
 
 function drawParent() {
@@ -89,16 +75,12 @@ function removeChildren() {
 	drawChildren();
 }
 
-function setTransSpeed(item, speed) {
-	item.css('transition', 'opacity ' + speed);
-	item.css('-moz-transition', 'opacity ' + speed);
-	item.css('-webkit-transition', 'opacity ' + speed);
-	item.css('-o-transition', 'opacity ' + speed);
-}
-
-function setTransNormal(item) {
-	item.css('transition', 'text-shadow 500ms, opacity 500ms');
-	item.css('-moz-transition', 'text-shadow 500ms, opacity 500ms');
-	item.css('-webkit-transition', 'text-shadow 500ms, opacity 500ms');
-	item.css('-o-transition', 'text-shadow 500ms, opacity 500ms');
+function bootstrap() {
+	$(document).ready( function() {
+    	currentObject = contents['mukti'];
+		drawChildren();
+        setTimeout(function (){
+        	$('#parentNode').removeClass('invisible');
+        }, 10);
+    });
 }
